5728c623fcbf4510eb074595623ccc9b9370b8ba
Chaoching Vang <chaoching.vang@gmail.com>
2021-10-22 16:52:12 -0500

    added images and description

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,54 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    {
+        id: 1,
+        title: Goat,
+        path: 'images/goat_small.jpg',
+        description: 'Photo of a goat taken at Glacier National Park.',
+        likes: 0
+    },
+
+    {
+        id: 2,
+        title: Unicorn, 
+        path: `images/unicorn.jpg`,
+        description: `Photo of a unicorn. Do unicorns neigh?`,
+        likes:0
+    },
+    {
+        id: 3,
+        title: Dragon, 
+        path: `images/dragon.jpg`,
+        description: `Photo of a dragon. A natural procrastinator as they like dragon things on.`,
+        likes:0
+    },
+    {
+        id: 4,
+        title: Mermaid, 
+        path: `images/mermaid.jpg`,
+        description: `Photo of a mermaid. Michael Phelps vs a Mermaid, who wins?`,
+        likes:0
+    },
+    {
+        id: 5,
+        title: Werewolf, 
+        path: `images/werewolf.jpg`,
+        description: `Photo of a werewolf. It's always werewolf, not how is wolf.`,
+        likes:0
+    },
+    {
+        id: 6,
+        title: Fairy, 
+        path: `images/fairy.jpg`,
+        description: `Photo of a fairy. `,
+        likes:0
+    },
+    {
+        id: 7,
+        title: Leprechaun, 
+        path: `images/leprechaun.jpg`,
+        description: `Photo of a leprechaun. `,
+        likes:0
+    },
 ];
 
 module.exports = galleryItems;
