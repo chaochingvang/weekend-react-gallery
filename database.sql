6f7b0d303071e56b6aad84a596f06e16a2af9968
Chaoching Vang <chaoching.vang@gmail.com>
2021-10-24 11:48:01 -0500

    created db on positco and added database.sql file

---
+++ database.sql
@@ -0,0 +1,20 @@
+--create database named 'react_gallery'
+
+CREATE TABLE "galleryList"(
+"id" SERIAL PRIMARY KEY,
+"title" VARCHAR(64) NOT NULL, 
+"path" VARCHAR(255) NOT NULL,
+"description" VARCHAR(255) NOT NULL,
+"likes" INT DEFAULT 0,
+"imgStatus" BOOLEAN DEFAULT TRUE
+);
+
+INSERT INTO "galleryList" ("title", "path", "description")
+VALUES
+('Goat', 'images/goat_small.jpg', 'Photo of a goat taken at Glacier National Park'),
+('Unicorn', 'images/unicorn.jpg', 'Photo of a unicorn. Do unicorns neigh?'),
+('Dragon', 'images/dragon.jpg', 'Photo of a dragon. A natural procrastinator as they like dragon things on.'),
+('Mermaid', 'images/mermaid.jpg', 'Photo of a mermaid. Michael Phelps vs a Mermaid, who wins?'),
+('Werewolf', 'images/werewolf.jpg', 'Photo of a werewolf. It is always werewolf, but never how is wolf.'),
+('Fairy', 'images/fairy.jpg', 'Photo of a fairy. They actually exist! I am fairy certain.'),
+('Leprechaun', 'images/leprechaun.jpg', 'Photo of a leprechaun. The most mysterious thing about these creature is actually the size of their pockets.');
